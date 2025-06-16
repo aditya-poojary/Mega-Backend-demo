fd13fc9201d3797707e7638af8e2d22f3d616c6c
Aditya Poojary <adityapoojary07@gmail.com>
2025-06-16 23:05:11 +0530

    feat: set up MongoDB connection and environment configuration
    - Updated package.json to include dotenv, express, and mongoose dependencies.
    - Modified dev script to use dotenv and experimental JSON modules.
    - Added constants.js to define the database name.
    - Created db/index.js to handle MongoDB connection using mongoose.
    - Implemented database connection logic in index.js and configured dotenv.

---
+++ src/index.js
@@ -0,0 +1,44 @@
+// require('dotenv').config({path: './env'})
+
+import connectDB from "./db/index.js"
+import dotenv from "dotenv"
+
+dotenv.config({
+    path:'./env'
+})
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+async () => {
+  try {
+    mongoose.connect(`${process.env.MONGODB_URI}`);
+    app.on("error", (error) => {
+      console.log("Error: ", error);
+      throw error;
+    });
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("Error: ", error);
+    throw error;
+  }
+};
+*/
