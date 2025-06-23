44dfc77e4f10dd1dc37d6691c853e13181560673
Aditya Poojary <adityapoojary07@gmail.com>
2025-06-23 18:58:36 +0530

    Do the CORS setting for best practices

--- src/db/index.js
+++ src/db/index.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants.js";
+import { DB_NAME } from "../constants.js"; 
 
 const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log(`\n MongoDB connected !! db host: ${connectionInstance.connection.host}`);
+        
     }
     catch (error) {
         console.log("MONGODB connection error", error);
--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@
 
 import connectDB from "./db/index.js"
 import dotenv from "dotenv"
+import app from "./app.js"
 
 dotenv.config({
   path: './env'
 })
 
-connectDB();
-
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000)
+    console.log("Database is connected!!",process.env.PORT)
+  })
+  .catch((err) => {
+    console.log("Error while connect!!", err)
+  })
 
 
 
