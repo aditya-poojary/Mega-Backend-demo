c7669b13b210a54b2ea8eb4ab53bc15eea4ba051
Aditya Poojary <adityapoojary07@gmail.com>
2025-07-29 19:41:15 +0530

    feat: implement JWT authentication middleware and enhance user login functionality

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,13 +1,15 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
-import {upload} from "../middlewares/multer.middleware.js"
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js"
+import { verify } from "jsonwebtoken";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/register").post(
     upload.fields([
         {
-            name:"avatar",
+            name: "avatar",
             maxCount: 1
         },
         {
@@ -17,5 +19,11 @@ router.route("/register").post(
     ]),
     registerUser
 )
+
+router.route("/login").post(loginUser)
+
+//secured routes
+router.route("/logout").post(verifyJWT, logoutUser)
+
 
 export default router
